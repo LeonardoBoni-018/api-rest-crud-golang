6af59b1ec5f54f65954d0e32265fc37347d243db
LeonardoBoni-018 <boni.leonardor@outlook.com>
2026-03-04 22:36:54 -0300

    Implementando models response e request em createUser

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,23 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/LeonardoBoni-018/api-rest-crud-golang/configuration/rest_err"
+	"github.com/LeonardoBoni-018/api-rest-crud-golang/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("Você chamou a rota de forma errada!")
-	c.JSON(err.Code, err)
+
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		rest_err := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s\n", err.Error()),
+		)
+		c.JSON(rest_err.Code, rest_err)
+		return
+	}
+	fmt.Println(userRequest)
 }
