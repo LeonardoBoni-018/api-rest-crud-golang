a2a8a9d8608da75ece0160475cee956902da07a7
LeonardoBoni-018 <boni.leonardor@outlook.com>
2026-03-05 22:43:32 -0300

    Adicionando validação para criação de usuário

---
+++ src/configuration/validation/validate_user.go
@@ -0,0 +1,57 @@
+package validation
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/LeonardoBoni-018/api-rest-crud-golang/configuration/rest_err"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	en_translation "github.com/go-playground/validator/v10/translations/en"
+)
+
+var (
+	Validate = validator.New()
+	transl   ut.Translator
+)
+
+func init() {
+	// Inicializa o tradutor
+	if val, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		en := en.New()
+		unt := ut.New(en, en)
+		transl, _ = unt.GetTranslator("en")
+		en_translation.RegisterDefaultTranslations(val, transl)
+	}
+}
+
+func ValidateUserError(validation_err error) *rest_err.RestErr {
+	var jsonErr *json.UnmarshalTypeError
+	var jsonValidationError validator.ValidationErrors
+
+	// Verifica se o erro é do tipo json.UnmarshalTypeError
+	// Erro de tipo
+	if errors.As(validation_err, &jsonErr) {
+		return rest_err.NewBadRequestError("Invalid field type")
+	} else if errors.As(validation_err, &jsonValidationError) {
+		// Erro de validação
+		errorsCauses := []rest_err.Causes{}
+
+		// Itera sobre os erros de validação
+		for _, e := range validation_err.(validator.ValidationErrors) {
+			cause := rest_err.Causes{
+				Message: e.Translate(transl),
+				Field:   e.Field(),
+			}
+
+			// Adiciona a causa ao slice de causas
+			errorsCauses = append(errorsCauses, cause)
+		}
+		return rest_err.NewBadRequestValidationError("Some fields are invalid", errorsCauses)
+	} else {
+		// Erro genérico
+		return rest_err.NewBadRequestError("Error trying to convert fields")
+	}
+}
--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,10 +1,11 @@
 package controller
 
 import (
-	"fmt"
+	"net/http"
 
-	"github.com/LeonardoBoni-018/api-rest-crud-golang/configuration/rest_err"
+	"github.com/LeonardoBoni-018/api-rest-crud-golang/src/configuration/validation"
 	"github.com/LeonardoBoni-018/api-rest-crud-golang/src/controller/model/request"
+	"github.com/LeonardoBoni-018/api-rest-crud-golang/src/controller/model/response"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,11 +14,18 @@ func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		rest_err := rest_err.NewBadRequestError(
-			fmt.Sprintf("There are some incorrect fields, error=%s\n", err.Error()),
-		)
-		c.JSON(rest_err.Code, rest_err)
+		errRest := validation.ValidateUserError(err)
+
+		c.JSON(errRest.Code, errRest)
 		return
 	}
-	fmt.Println(userRequest)
+
+	response := response.UserResponse{
+		ID:    "test",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+
+	c.JSON(http.StatusOK, response)
 }
